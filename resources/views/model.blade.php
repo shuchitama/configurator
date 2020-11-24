0dc5cf7a5ffa654f72aca4756946e7c892b32282
Shuchita Mahey <shuchita@live.com>
2020-11-24 15:12:47 -0800

    show model on page

--- resources/views/model.blade.php
+++ resources/views/model.blade.php
@@ -5,10 +5,31 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Model</title>
+  <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+  <style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  .imgbox {
+    display: grid;
+    height: 100%;
+  }
+
+  .center-fit {
+    width: 100%;
+    height: 100vh;
+    margin: auto;
+  }
+  </style>
 </head>
 
 <body>
-
+  <div class="imgbox">
+    <model-viewer class="center-fit" src="storage/models/RobotExpressive.glb" auto-rotate camera-controls>
+    </model-viewer>
+  </div>
 </body>
 
 </html>
