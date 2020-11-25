4426af683aef17ebb87ccf0dcdcc8593b66e07fd
Shuchita Mahey <shuchita@live.com>
2020-11-25 13:59:28 -0800

    add orbit controls

--- resources/views/three.blade.php
+++ resources/views/three.blade.php
@@ -22,7 +22,7 @@
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 5;
-  camera.position.y = 2;
+  camera.position.y = 5;
   const renderer = new THREE.WebGLRenderer({
     antialias: true
   });
@@ -41,6 +41,11 @@
   const raycaster = new THREE.Raycaster();
   const mouse = new THREE.Vector2();
 
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 0.5, 0);
+  controls.update();
+  controls.enablePan = true;
+  controls.enableDamping = true;
 
   const loader = new GLTFLoader();
   // Load a glTF resource
