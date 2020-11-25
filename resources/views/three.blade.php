a556edc546832ba7d8a3e96fe9e2f4e53bc0c3c6
Shuchita Mahey <shuchita@live.com>
2020-11-25 13:43:06 -0800

    GLTF imported and used successfully

--- resources/views/three.blade.php
+++ resources/views/three.blade.php
@@ -8,12 +8,21 @@
 </head>
 
 <body>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/102/three.js"></script>
+
   Three model here - <?= $name ?>
-  <script>
+  <script type="module">
+  import * as THREE from "https://unpkg.com/three@0.123.0/build/three.module.js"
+  import {
+    GLTFLoader
+  } from "https://unpkg.com/three@0.123.0/examples/jsm/loaders/GLTFLoader.js"
+  import {
+    OrbitControls
+  } from 'https://unpkg.com/three@0.123.0/examples/jsm/controls/OrbitControls.js';
+  // import * as THREE from "../../node_modules/three/build/three.module.js"
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 5;
+  camera.position.y = 2;
   const renderer = new THREE.WebGLRenderer({
     antialias: true
   });
@@ -33,60 +42,35 @@
   const mouse = new THREE.Vector2();
 
 
-  let geometry = new THREE.BoxGeometry(1, 1, 1);
-  let material = new THREE.MeshLambertMaterial({
-    color: 0xF7F7F7
-  });
-  // let mesh = new THREE.Mesh(geometry, material);
+  const loader = new GLTFLoader();
+  // Load a glTF resource
+  loader.load('storage/models/<?= $name ?>', function(gltf) {
 
-  // scene.add(mesh)
+      scene.add(gltf.scene);
 
-  meshX = -10
-  for (let i = 0; i < 10; i++) {
-    let mesh = new THREE.Mesh(geometry, material);
-    mesh.position.x = (Math.random() - 0.5) * 10;
-    mesh.position.y = (Math.random() - 0.5) * 10;
-    mesh.position.z = (Math.random() - 0.5) * 10;
-    scene.add(mesh);
-    meshX += 1;
-  }
+      gltf.animations; // Array<THREE.AnimationClip>
+      gltf.scene; // THREE.Group
+      gltf.scenes; // Array<THREE.Group>
+      gltf.cameras; // Array<THREE.Camera>
+      gltf.asset; // Object
 
-  let light = new THREE.PointLight(0xFFFFFF, 1, 1000)
-  light.position.set(0, 0, 0);
-  scene.add(light)
+    },
+    undefined,
+    function(e) {
+      console.error(e);
+    });
 
-  light = new THREE.PointLight(0xFFFFFF, 2, 1000)
-  light.position.set(0, 0, 25);
-  scene.add(light)
+
+  const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+  dirLight.position.set(5, 2, 8);
+  scene.add(dirLight);
 
   const render = function() {
     requestAnimationFrame(render);
     renderer.render(scene, camera);
   }
 
-  // function onMouseMove(event) {
-  //   event.preventDefault();
-
-  //   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  //   mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-
-  //   raycaster.setFromCamera(mouse, camera);
-
-  //   const intersects = raycaster.intersectObjects(scene.children, true);
-  //   for (let i = 0; i < intersects.length; i++) {
-  //     // intersects[i].object.material.color.set(0xff0000)
-  //     this.tl = new TimelineMax();
-  //     this.tl.to(intersects[i].object.scale, 1, { x: 2, ease: Expo.easeOut })
-  //     this.tl.to(intersects[i].object.scale, 0.5, { x: .5, ease: Expo.easeOut })
-  //     this.tl.to(intersects[i].object.position, .5, { x: 2, ease: Expo.easeOut })
-  //     this.tl.to(intersects[i].object.rotation, .5, { y: Math.PI * .5, ease: Expo.easeOut }, "=-1.5")
-  //   }
-  // }
-
   render();
-
-
-  window.addEventListener('mousemove', onMouseMove)
   </script>
 </body>
 
