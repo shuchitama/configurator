ec7a454d909c8e8526dac6e4b43ad144b732607c
Shuchita Mahey <shuchita@live.com>
2020-12-01 13:11:42 -0800

    get dimensions of a bounding box

--- resources/views/three.blade.php
+++ resources/views/three.blade.php
@@ -20,11 +20,14 @@
   import {
     OrbitControls
   } from 'https://unpkg.com/three@0.123.0/examples/jsm/controls/OrbitControls.js';
-  // import * as THREE from "../../node_modules/three/build/three.module.js"
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 5;
-  camera.position.y = 5;
+  // camera.position.z = 10;
+  // camera.position.y = 10;
+  // camera.position.y = 10;
+  const helper = new THREE.CameraHelper(camera);
+  scene.add(helper);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
   const renderer = new THREE.WebGLRenderer({
     antialias: true
   });
@@ -49,23 +52,35 @@
   controls.enablePan = true;
   controls.enableDamping = true;
 
+  let model = new THREE.Object3D();
   const loader = new GLTFLoader();
   // Load a glTF resource
   loader.load('storage/models/<?= $name ?>', function(gltf) {
+      model = gltf.scene;
+      // model.scale.set(1, 1, 1);
+      // model.position.x = 0;
+      // model.position.y = 0;
+      scene.add(model);
 
-      scene.add(gltf.scene);
+      const box = new THREE.Box3().setFromObject(model);
+      let vector = new THREE.Vector3();
+      console.log("BOX MIN:", box.min);
+      console.log("BOX MAX:", box.max, box.getSize(vector));
+      console.log("BOX SIZE:", box.getSize(vector));
 
-      gltf.animations; // Array<THREE.AnimationClip>
-      gltf.scene; // THREE.Group
-      gltf.scenes; // Array<THREE.Group>
-      gltf.cameras; // Array<THREE.Camera>
-      gltf.asset; // Object
+      // gltf.animations; // Array<THREE.AnimationClip>
+      // gltf.scene; // THREE.Group
+      // gltf.scenes; // Array<THREE.Group>
+      // gltf.cameras; // Array<THREE.Camera>
+      // gltf.asset; // Object
 
     },
     undefined,
     function(e) {
       console.error(e);
     });
+
+
 
 
   let dirLight = new THREE.DirectionalLight(0xffffff, 1);
